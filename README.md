2b3ca95ab587eabd0cdcc51cefe83ce49e24631f
cloudwebrtc <duanweiwei1982@gmail.com>
2023-11-27 10:50:52 +0800

    first commit.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # dart-video-element-flicker-demo
+
+## How to develop
+
+* `dart pub get`
+* `dart pub global activate webdev`
+* `webdev serve --auto=refresh`
---
+++ web/main.dart
@@ -0,0 +1,27 @@
+import 'dart:html' as html;
+
+html.MediaStream? _localStream;
+
+void main() async {
+  html.document.querySelector('#addVideo')?.onClick.listen((event) async {
+    print('add video');
+    if (_localStream == null) {
+      _localStream = await html.window.navigator.getUserMedia(video: true);
+    }
+    var local = html.document.querySelector('#local_videos');
+    var localVideo = html.VideoElement();
+    localVideo.style.width = '240px';
+    localVideo.style.height = '180px';
+    localVideo.style.border = '1px solid black';
+    localVideo.muted = true;
+    localVideo.autoplay = true;
+    local!.append(localVideo);
+    localVideo.srcObject = _localStream;
+  });
+
+  html.document.querySelector('#removeVideo')?.onClick.listen((event) async {
+    print('remove video');
+    var local = html.document.querySelector('#local_videos');
+    local!.children.last.remove();
+  });
+}
